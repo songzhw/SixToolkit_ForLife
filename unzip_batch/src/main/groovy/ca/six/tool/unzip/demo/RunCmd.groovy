8112b9003c6fca44586e34be48e610595413e663
songzhw <songzhw2012@gmail.com>
2018-03-27 10:07:07 -0400

    be careful about the file separator in Windows

--- unzip_batch/src/main/groovy/ca/six/tool/unzip/demo/RunCmd.groovy
+++ unzip_batch/src/main/groovy/ca/six/tool/unzip/demo/RunCmd.groovy
@@ -1,7 +1,10 @@
 package ca.six.tool.unzip.demo
 
+// this is an example of running WINDOWS cmd
 def runCmd(){
-    println "szw"
+//    def process = Runtime.getRuntime().exec("explorer E:/temp") //fail, 会走到"我的文档", 即默认目录里去
+    def process = Runtime.getRuntime().exec("explorer E:\\temp")
+
 }
 
 runCmd()
