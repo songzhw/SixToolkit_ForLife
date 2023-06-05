f322f1ea6099b0326272c92895a46e902b628705
songzhw <songzhw2012@gmail.com>
2023-06-05 16:15:23 -0400

    not working?!

--- Tools_On_Mac/lib/apps/rename/BulkRenamePage.dart
+++ Tools_On_Mac/lib/apps/rename/BulkRenamePage.dart
@@ -1,13 +1,38 @@
+import 'package:filepicker_macos/filepicker_macos.dart';
 import 'package:flutter/material.dart';
 
 import '../../Commons.dart';
 
 class BulkRenamePage extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: appbar("Bulk Re-name"),
-            body: Container(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appbar("Bulk Re-name"),
+      body: Column(
+        children: [
+          TextButton(onPressed: f, child: Text("ccc")),
+        ],
+      ),
+    );
+  }
+
+  f() async {
+    print('11');
+    final ss = await FilepickerMacos.pick(
+        canChooseFiles: true,
+        allowsMultipleSelection: true,
+        directoryURL: "~/Downloads",
+        prompt: "hello"
+    );
+    print('ss = ${ss}');
+  }
 }
+
+/*
+1.
+final dir = await getApplicationDocumentsDirectory();
+print('dir = ${dir.path}'); //=> /Users/szw/Library/Containers/ca.six.tools.mac.toolsOnMac/Data/Documents
+
+final dir = await getDownloadsDirectory();
+print('dir = ${dir?.path}'); //=> /Users/szw/Library/Containers/ca.six.tools.mac.toolsOnMac/Data/Downloads
+ */
--- Tools_On_Mac/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Tools_On_Mac/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import filepicker_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilepickerMacosPlugin.register(with: registry.registrar(forPlugin: "FilepickerMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
